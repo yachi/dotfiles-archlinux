4ef174401511044b74f3d2808b54d360db46cf1d
Yachi Lo <yaachi@gmail.com>
2014-02-23 22:17:03 +0800

    move shell=bash to vimrc.before

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ ln -svf $(realpath ./.gitconfig) ~/.gitconfig
 ln -svf $(realpath ./.gemrc) ~/
 ln -svf $(realpath ./.janus) ~/
 ln -svf $(realpath ./.janus/.vimrc.after) ~/
+ln -svf $(realpath ./.janus/.vimrc.before) ~/
 ln -svf $(realpath ./powerline-shell.py) ~/.powerline-shell.py
