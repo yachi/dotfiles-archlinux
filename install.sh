af0dd8d506ff37205b35363585ad3a02316b028b
Yachi Lo <yaachi@gmail.com>
2014-02-23 22:15:38 +0800

    add gemrc

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 [ -L ~/.config/fish ] || ln -svf $(realpath ./fish) ~/.config/fish
 
 ln -svf $(realpath ./.gitconfig) ~/.gitconfig
+ln -svf $(realpath ./.gemrc) ~/
 ln -svf $(realpath ./.janus) ~/
 ln -svf $(realpath ./.janus/.vimrc.after) ~/
 ln -svf $(realpath ./powerline-shell.py) ~/.powerline-shell.py
