d69c0f201381b66f6b86376406d5d398795c219d
Yachi Lo <yaachi@gmail.com>
2014-05-19 00:59:52 +0800

    add dotvim

--- install.sh
+++ install.sh
@@ -5,3 +5,7 @@
 ln -svf $(realpath ./.gitconfig) ~/.gitconfig
 ln -svf $(realpath ./.gemrc) ~/
 ln -svf $(realpath ./.aria2) ~/
+ln -svf $(realpath ./.vimrc) ~/
+
+test -d ~/.vim || ln -svf $(realpath ./dotvim) ~/.vim
+git submodule update --init --recursive
