269fd7301c846aa52dbcff5ea6eea9bf60af3760
Yachi Lo <yaachi@gmail.com>
2016-07-06 11:31:46 +0800

    update omf init.fish

--- omf/init.fish
+++ omf/init.fish
@@ -3,10 +3,10 @@ set PATH /usr/local/bin $PATH
 set PATH ~/.local/bin $PATH
 set PATH /opt/android-sdk/platform-tools $PATH
 set PATH /opt/google-cloud-sdk/bin $PATH
-set -x GOPATH $HOME/go
-set -x GOARCH amd64
-set -x GOOS linux
-set -x PATH $GOPATH/bin $PATH
+set -gx GOPATH $HOME/go
+set -gx GOARCH amd64
+set -gx GOOS linux
+set -gx PATH $GOPATH/bin $PATH
 
 # init z
 source ~/.dotfiles/z-fish/z.fish
@@ -91,7 +91,7 @@ set fish_user_abbreviations $fish_user_abbreviations 'gca=git commit -av'
 set fish_user_abbreviations $fish_user_abbreviations 'gco=git checkout'
 set fish_user_abbreviations $fish_user_abbreviations 'gup=git pull --rebase'
 set fish_user_abbreviations $fish_user_abbreviations 'gfg=git ls-files|grep'
-set fish_user_abbreviations $fish_user_abbreviations 'gsup=git submodule update —init'
+set fish_user_abbreviations $fish_user_abbreviations 'gsup=git submodule update --init'
 set fish_user_abbreviations $fish_user_abbreviations 'gf=git flow'
 set fish_user_abbreviations $fish_user_abbreviations 'gffs=git flow feature start'
 set fish_user_abbreviations $fish_user_abbreviations 'gffp=git flow feature publish'
@@ -106,6 +106,8 @@ set fish_user_abbreviations $fish_user_abbreviations 'drw=docker-compose run --r
 set fish_user_abbreviations $fish_user_abbreviations 'drws=docker-compose run --rm web bin/spring'
 set fish_user_abbreviations $fish_user_abbreviations 'le=less -R'
 set fish_user_abbreviations $fish_user_abbreviations 'lsd=lynx -stdin -dump'
+set fish_user_abbreviations $fish_user_abbreviations 'os=overcommit --sign'
+set fish_user_abbreviations $fish_user_abbreviations 'od=env OVERCOMMIT_DISABLE=1'
 
 # osx pbcopy
 alias pbcopy='xsel --clipboard --input'
@@ -123,6 +125,16 @@ end
 function prax_forward
   sudo iptables -t nat -I OUTPUT -p tcp -o lo --dport 80 -j REDIRECT --to-ports 20559
 end
+
+function drwr
+  docker-compose run --rm web bin/spring rspec (git status --short | awk "{print \$2}" | grep -E "^spec/.*_spec.rb")
+end
+
+function drwdb
+  docker-compose run                   --rm web bin/spring rake db:setup
+  docker-compose run -e RAILS_ENV=test --rm web bin/spring rake db:setup
+end
+
 alias gwip='git add -A; git ls-files --deleted -z | xargs -r -0 git rm; git commit -m "wip"'
 alias gunwip='git log -n 1 | grep -q -c wip; and git reset HEAD~1'
 
